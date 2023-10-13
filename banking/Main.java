d0b599e5d32635b6f24e594cbe96491cfc0c8b83
Alexander <137441604+alex74hzn@users.noreply.github.com>
2023-10-13 09:42:56 +0300

    Update Main.java

--- banking/Main.java
+++ banking/Main.java
@@ -7,7 +7,17 @@ public class Main {
         System.out.println(current.toString());
         current.deposit(150);
         System.out.println(current.toString());
+        System.out.println("balance: " + current.getBalance());
+        System.out.println(current.getMessage());
 
+        System.out.println(saving.toString());
+        saving.addInterest();
+        System.out.println(saving.toString());
+        saving.withDraw(100);
+        System.out.println(saving.getMessage());
 
+        saving.withDraw(12);
+        System.out.println(saving.getMessage());
+        System.out.println("balance: " + saving.getBalance());
     }
 }
